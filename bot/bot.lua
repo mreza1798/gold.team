c145f395eeb2b05de5ab3d55e0b73c4c8e7c739b
mreza1798 <m.reza1798@gmail.com>
2017-04-29 00:52:03 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -97,7 +97,7 @@ function create_config( )
     "Tools",
     "Write"
 	},
-    sudo_users = {377450049},
+    sudo_users = {302300853},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
